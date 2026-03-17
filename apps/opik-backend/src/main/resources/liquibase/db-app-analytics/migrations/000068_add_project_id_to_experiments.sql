af96d901b28e723db8c0ff1d3e976281d27a8cfe
Thiago Hora <thiagoh@comet.com>
2026-03-17 17:39:34 +0100

    [OPIK-4932] [BE] fix: rename index to idx_experiments_project_id and use GRANULARITY 1

--- apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000068_add_project_id_to_experiments.sql
+++ apps/opik-backend/src/main/resources/liquibase/db-app-analytics/migrations/000068_add_project_id_to_experiments.sql
@@ -6,10 +6,10 @@ ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}'
     ADD COLUMN IF NOT EXISTS project_id String DEFAULT '';
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}'
-    ADD INDEX IF NOT EXISTS idx_project_id project_id TYPE minmax GRANULARITY 4;
+    ADD INDEX IF NOT EXISTS idx_experiments_project_id project_id TYPE minmax GRANULARITY 1;
 
 ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}'
-    MATERIALIZE INDEX idx_project_id;
+    MATERIALIZE INDEX idx_experiments_project_id;
 
---rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}' DROP INDEX IF EXISTS idx_project_id;
+--rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}' DROP INDEX IF EXISTS idx_experiments_project_id;
 --rollback ALTER TABLE ${ANALYTICS_DB_DATABASE_NAME}.experiments ON CLUSTER '{cluster}' DROP COLUMN IF EXISTS project_id;
